1735efda4d81daa0ffcd287f10f26f05b3aa2fe9
matheus.s.pinheiro@live.com <matheus.s.pinheiro@live.com>
2023-06-20 20:28:28 -0300

    responsivity-fix

--- src/app/components/CaseStudies.tsx
+++ src/app/components/CaseStudies.tsx
@@ -18,64 +18,57 @@ function CaseStudies() {
         initial={{ opacity: 0 }}
         transition={{ duration: 2 }}
         whileInView={{ opacity: 1 }}
-        className="relative z-20 flex w-full snap-x snap-mandatory overflow-y-hidden overflow-x-scroll scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-red-50/70"
+        className="relative z-20 flex w-full snap-x snap-mandatory overflow-y-hidden overflow-x-scroll scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-red-50/70"
       >
         {projects.map((project) => (
-          <div
-            className="flex h-screen w-screen flex-shrink-0 snap-center flex-col items-center justify-center space-y-5 p-20 md:p-44"
-            key={project}
-          >
-            <motion.div
-              initial={{
-                y: -300,
-                opacity: 0,
-              }}
-              transition={{ duration: 1.5 }}
-              whileInView={{
-                opacity: 1,
-                y: 0,
-              }}
-              viewport={{ once: true }}
-            >
-              <Image
-                className="rounded-lg"
-                width={390}
-                src={DinDinImage}
-                alt="image"
-              />
-            </motion.div>
+          <div key={project} id={`${project}`}>
+            <div className="flex h-screen w-screen flex-shrink-0 snap-center flex-col items-center justify-center space-y-5 p-20 md:p-44">
+              <motion.div
+                initial={{
+                  y: -300,
+                  opacity: 0,
+                }}
+                transition={{ duration: 1.5 }}
+                whileInView={{
+                  opacity: 1,
+                  y: 0,
+                }}
+                viewport={{ once: true }}
+              >
+                <Image
+                  className="h-full rounded-lg object-cover md:w-[390px]"
+                  width={390}
+                  src={DinDinImage}
+                  alt="image"
+                />
+              </motion.div>
 
-            <motion.div
-              initial={{
-                y: 300,
-                opacity: 0,
-              }}
-              transition={{ duration: 1.5 }}
-              whileInView={{
-                opacity: 1,
-                y: 0,
-              }}
-              viewport={{ once: true }}
-              className="w-screen p-4 md:max-w-[1000px]"
-            >
-              <h4 className="text-lg text-red-50">
-                Estudo de caso {project} de {projects.length}: DinDin
-              </h4>
-              <p className="text-gray-100 ">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Expedita quibusdam magnam natus, eveniet quia quas reiciendis
-                enim sint hic libero, eius alias optio dolorem consequuntur quam
-                nam! Vitae incidunt commodi ut quibusdam ad iure reiciendis vero
-                quidem aspernatur sint officiis eligendi recusandae repellat
-                soluta possimus, quae suscipit. Harum vero voluptate magni iure
-                sapiente voluptatibus vitae sit animi! Nihil dicta eligendi rem
-                hic repudiandae impedit nobis, saepe quos dignissimos,
-                consectetur sit quibusdam voluptates facere repellat quo ipsa
-                voluptatum totam aperiam, cumque obcaecati eum cum inventore.
-                Fugit cumque molestiae, animi dolorem laboriosam exercitationem
-                laborum modi. Sed iusto, quis repellat reiciendis cum ea.
-              </p>
-            </motion.div>
+              <motion.div
+                initial={{
+                  y: 300,
+                  opacity: 0,
+                }}
+                transition={{ duration: 1.5 }}
+                whileInView={{
+                  opacity: 1,
+                  y: 0,
+                }}
+                viewport={{ once: true }}
+                className="w-screen p-4 md:max-w-[1000px]"
+              >
+                <h4 className="text-lg text-red-50">
+                  Estudo de caso {project} de {projects.length}: DinDin
+                </h4>
+                <p className="overflow-y-auto text-gray-100">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et
+                  excepturi vel ratione quis ex tempore, doloribus dolorem ipsum
+                  velit ipsam modi cupiditate eum aliquam vero dolorum
+                  consequuntur, natus, quae eligendi sapiente! Illum quos, natus
+                  amet distinctio consequuntur quam iste, unde nulla dolorum
+                  aspernatur molestiae accusantium.
+                </p>
+              </motion.div>
+            </div>
           </div>
         ))}
       </motion.div>
